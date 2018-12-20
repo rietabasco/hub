eed2ce38892769da26f5edd680fc532cb941e29a
Jairo <olan.mayo05@gmail.com>
2018-12-19 19:00:09 -0600

    agregando ruta2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
+  config.action_mailer.default_url_options = { :host => 'rietabasco-id.herokuapp.com' }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
