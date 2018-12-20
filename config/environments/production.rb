f662dffb414ad250acd4672ae03a95cefdc50387
Jairo <olan.mayo05@gmail.com>
2018-12-19 19:25:38 -0600

    Agregando mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,20 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
   config.action_mailer.default_url_options = { :host => 'rietabasco-id.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default :charset => "utf-8"
+
+
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :authentication => :plain,
+  :domain => 'gmail.com',
+  :user_name => 'pruebas.h7@gmail.com',
+  :password => 'wpyqeusfeukgigyl'
+}
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
